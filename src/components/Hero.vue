28be2ce849e2dcc4b3274ed303434a10ddc32a54
Haris Maulana <99maulanaaris@gmail.com>
2020-10-12 21:20:05 +0700

    Menambahkan File Baru

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row mt-4">
+      <div class="col">
+          <div class="d-flex h-100">
+              <div class="justify-content-center align-self-center">      
+              <h2><strong>Delicious</strong> Food Menu <br> in your gadget</h2>
+              <p>Harga Murah Enak Dan Berkualitas , <strong>PESAN SEKARANG</strong></p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-bar-right></b-icon-arrow-bar-right>
+                Klik Disini
+              </button>
+              </div>
+          </div>
+      </div>
+      <div class="col">
+          <img src="../assets/img/banner.png" width="100%">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+    name: 'Hero'
+}
+</script>
